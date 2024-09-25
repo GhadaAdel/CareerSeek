dc992c175cd82664e66703f0e9ec74e27e727f42
GhadaAdel <gh.adel.aa@gmail.com>
2024-09-25 22:54:39 +0300

    forms components - Regsitration

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -11,7 +11,7 @@
         rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-black text-white font-hanken-grotesk">
+<body class="bg-black text-white font-hanken-grotesk pb-20">
 
 <!-- div as a wrapper for the nav bar and the main area after it -->
 
